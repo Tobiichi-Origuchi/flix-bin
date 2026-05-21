dc1e909497e3589fcef362b89ab5c70c5bc6ab19
tobiichioriguchi <tobiichioriguchi@gmail.com>
2026-05-21 16:25:22 +0800

    fix typo

--- scripts/build_and_publish_linux.sh
+++ scripts/build_and_publish_linux.sh
@@ -146,7 +146,8 @@ sha256sums=(
 package() {
   bsdtar -xf data.tar.zst -C "\$pkgdir"
   install -Dm644 "\$pkgdir"/opt/flix/data/flutter_assets/assets/data/flix_privacy.md "\$pkgdir"/usr/share/licenses/\${pkgname}/flix_privacy.md
-  ln -s "\$pkgdir"/opt/flix/flix "\$pkgdir"/usr/bin/flix
+  install -d "\$pkgdir/usr/bin"
+  ln -s /opt/flix/flix "\$pkgdir"/usr/bin/flix
   sed -i \
     -e "s/Icon=com.ifreedomer.flix/Icon=flix/" \
     -e "s/Exec=flix %F/Exec=\/opt\/flix\/flix %F/" \
